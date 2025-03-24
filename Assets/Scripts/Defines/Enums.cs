56cb9a15625528d0f565caaaf29026110b8562b3
민지규 <jigyu12@gmail.com>
2025-03-24 16:52:40 +0900

    Add Temp Script PlaneToGridMap

--- Assets/Scripts/Defines/Enums.cs
+++ Assets/Scripts/Defines/Enums.cs
@@ -1,9 +1,36 @@
 public enum WayType
 {
-    Straight, 
+    Straight = 0, 
     Left,
     Right,
     UnavoidableWall,
     
-    Count,
+    Count
 }
+
+public enum RewardType
+{
+    TestRewardCoin = -1,
+    
+    BronzeCoin = 0,
+    SilverCoin,
+    GoldCoin,
+    PlatinumCoin,
+    DiamondCoin,
+    Human,
+    
+    Count
+}
+
+public enum PenaltyType
+{
+    TestPenaltyCoin = -1,
+    
+    GhostCoin = 0,
+    PoisonCoin,
+    SkullCoin,
+    FireCoin,
+    BlackHoleCoin,
+    
+    Count
+} 
