927a28077fcedc2f8e4804dd5f9e9008821e2530
박민재 <wer9384@naver.com>
2025-03-25 20:15:50 +0900

    플레이어 조작 개선 점프 이동 막기

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -126,12 +126,14 @@ public class PlayerMove : MonoBehaviour
 
     void MoveLeft()
     {
+        if (isJumping) return;
         wayIndex = Mathf.Clamp(wayIndex - 1, 0, 2);
         targetPosition = way.WayIndexToPosition(wayIndex);
     }
 
     void MoveRight()
     {
+        if (isJumping) return;
         wayIndex = Mathf.Clamp(wayIndex + 1, 0, 2);
         targetPosition = way.WayIndexToPosition(wayIndex);
     }
@@ -183,7 +185,9 @@ public class PlayerMove : MonoBehaviour
         {
             isJumping = false;
             verticalVelocity = 0f;
-
+            Vector3 newPosition = transform.position;
+            newPosition.y = 0f;
+            transform.position = newPosition;
             if (animator != null)
             {
                 animator.SetBool("Jump", false);
