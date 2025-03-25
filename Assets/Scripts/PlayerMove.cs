648e2a3d786584c76209928e725fb483126cd60c
민지규 <jigyu12@gmail.com>
2025-03-25 16:16:12 +0900

    Update PlayerMove.cs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -20,7 +20,6 @@ public class PlayerMove : MonoBehaviour
     private Vector2 swipeStart;
     private Vector2 currentTouchPosition;
 
-    public MapSpawn mapSpawner;
     public GameObject turnPivot;
 
     public Action<Vector3, float> onRotate;
