d6e687f4bf74d1ebfaea5b326a5156a9d35c3e5e
KALI-UM <1998khj@gmail.com>
2025-03-31 16:43:30 +0900

    맵 생성로직 변경으로 인해 중단

--- Assets/Scripts/Features/FollowCamara.cs
+++ Assets/Scripts/Features/FollowCamara.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class FollowCamara : MonoBehaviour
 {
-    public Transform targetPivot;
-    public float rotationSpeed = 5f;
+    public GameObject target;
+    public float rayLength;
+
+    public LayerMask ignoreLayers;
 
 
     private void LateUpdate()
     {
-        Vector3 targetPosition = targetPivot.position;
-        Quaternion targetRotation = targetPivot.rotation;
+        RaycastHit[] hits = Physics.RaycastAll(transform.position, target.transform.position, rayLength, ignoreLayers.value);
+        foreach(var ignoreObj in hits)
+        {
+            ignoreObj.collider.gameObject.SetActive(false);
+        }
 
-        //transform.position = Vector3.Lerp(transform.position, targetPosition, rotationSpeed * Time.deltaTime);
-        transform.position = targetPosition;
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, target.transform.position);
+    }
 }
