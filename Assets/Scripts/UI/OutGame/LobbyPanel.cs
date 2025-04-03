a721f8e5aaa4889782faf5dc993fd7272677a662
박민재 <wer9384@naver.com>
2025-04-03 20:13:29 +0900

    빌드V2 최종

--- Assets/Scripts/UI/OutGame/LobbyPanel.cs
+++ Assets/Scripts/UI/OutGame/LobbyPanel.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 public class LobbyPanel : MonoBehaviour
 {
     [SerializeField] private Button gameStartButton;
@@ -10,9 +11,30 @@ public class LobbyPanel : MonoBehaviour
         gameStartButton.onClick.RemoveAllListeners();
         gameStartButton.onClick.AddListener(OnGameStartButtonClicked);
     }
-    
+
     private void OnGameStartButtonClicked()
     {
-        SceneManagerEx.Instance.LoadScene("RunCopy");
+        AnimalDatabaseLoader loader = FindObjectOfType<AnimalDatabaseLoader>();
+        if (loader != null)
+        {
+            loader.LoadDataFromCSV();
+        }
+
+        AnimalDatabase database = GameDataManager.Instance.GetAnimalDatabase();
+        if (database == null)
+        {
+            Debug.LogError("AnimalDatabase를 찾을 수 없습니다!");
+            return;
+        }
+        List<int> runnerIDs = new List<int>();
+        foreach (AnimalStatus animal in database.Animals)
+        {
+            runnerIDs.Add(animal.AnimalID); // Id없어서 AnimalId로 햇음
+        }
+        GameDataManager.Instance.SetRunnerIDs(runnerIDs);
+
+        // ✅ 로딩씬으로 전환
+        SceneManager.LoadScene("LoadingScene", LoadSceneMode.Single);
     }
+
 }
