03c11d5b7d110cab5cebe1fc0e7efe14d92999aa
민지규 <jigyu12@gmail.com>
2025-04-09 20:31:22 +0900

    Add event To Action

--- Assets/Scripts/UI/OutGame/MenuPanel.cs
+++ Assets/Scripts/UI/OutGame/MenuPanel.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class MenuPanel : MonoBehaviour
 {
-    public static Action<SwitchableCanvasType> onMenuBottomButtonClicked;
+    public static event Action<SwitchableCanvasType> onMenuBottomButtonClicked;
     
     [SerializeField] private Button shopBottomButton;
     [SerializeField] private Button lobbyBottomButton;
