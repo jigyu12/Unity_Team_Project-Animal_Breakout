f6306d69da67877693497210de0fcb15c7016b4c
KALI-UM <1998khj@gmail.com>
2025-04-09 10:47:03 +0900

    Update EffectManager.cs

--- Assets/Scripts/Managers/EffectManager.cs
+++ Assets/Scripts/Managers/EffectManager.cs
@@ -2,7 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class EffectManager : InGameManager
 {
    
+=======
+public class EffectManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+>>>>>>> Stashed changes
 }
