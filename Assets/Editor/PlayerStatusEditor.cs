46916884435159ad2bbcb6b32646c2f536b8aeaf
KALI-UM <1998khj@gmail.com>
2025-04-17 11:18:20 +0900

    Update PlayerStatusEditor.cs

--- Assets/Editor/PlayerStatusEditor.cs
+++ Assets/Editor/PlayerStatusEditor.cs
@@ -24,5 +24,18 @@ public class PlayerStatusEditor : Editor
             playerStatus.ToggleInvincible();
             Debug.Log($"무적 상태 토글: {(playerStatus.IsInvincible ? "ON" : "OFF")}");
         }
+
+        //GUILayout.Label("");
+        //Editor editor = Editor.CreateEditor(playerStatus.statData);
+
+        //GUILayout.BeginHorizontal();
+        //GUILayout.Label("Reset to CSV data");
+        ////데이터 테이블 기존 값으로 
+        //if (GUILayout.Button("Revert"))
+        //{
+        //    dataEditor.currentEnemyData.SetData(DataTableManager.EnemyTable.Get(dataEditor.currentEnemyData.Id));
+        //}
+        //GUILayout.EndHorizontal();
     }
 }
+
